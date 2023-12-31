a12d4e9e30e2b550274165e0aa103436d8a0abaa
mariinkys <kysgeneral.nrgua@slmail.me>
2023-12-31 20:09:46 +0100

    end of video tutorial

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -1,12 +1,25 @@
+mod components;
+mod styles;
+mod systems;
+
 use bevy::prelude::*;
+
+use crate::AppState;
+
+use self::systems::{interactions::*, layout::*};
 
 pub struct MainMenuPlugin;
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(main_menu);
+        app
+            //On Enter State
+            .add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+            //Systems
+            .add_systems(
+                (interact_with_play_button, interact_with_quit_button)
+                    .in_set(OnUpdate(AppState::MainMenu)),
+            )
+            //On Exit State
+            .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)));
     }
-}
-
-pub fn main_menu() {
-    println!("Main Menu");
 }
---
+++ src/main_menu/systems/layout.rs
@@ -0,0 +1,118 @@
+use bevy::prelude::*;
+
+use crate::main_menu::{
+    components::{MainMenu, PlayButton, QuitButton},
+    styles::{
+        get_button_text_style, BUTTON_STYLE, IMAGE_STYLE, MAIN_MENU_STYLE, NORMAL_BUTTON_COLOR,
+        TITLE_STYLE,
+    },
+};
+
+pub fn spawn_main_menu(mut cmd: Commands, asset_server: Res<AssetServer>) {
+    let _main_menu_entity = build_main_menu(&mut cmd, &asset_server);
+}
+
+pub fn despawn_main_menu(mut cmd: Commands, main_menu_query: Query<Entity, With<MainMenu>>) {
+    if let Ok(main_menu_entity) = main_menu_query.get_single() {
+        cmd.entity(main_menu_entity).despawn_recursive(); //Despawn the parent and all of it's children
+    }
+}
+
+pub fn build_main_menu(cmd: &mut Commands, asset_server: &Res<AssetServer>) -> Entity {
+    let main_menu_entity = cmd
+        .spawn((
+            NodeBundle {
+                style: MAIN_MENU_STYLE,
+                //background_color: Color::RED.into(),
+                ..default()
+            },
+            MainMenu {},
+        ))
+        .with_children(|parent| {
+            //TITLE
+            parent
+                .spawn(NodeBundle {
+                    style: TITLE_STYLE,
+                    ..default()
+                })
+                .with_children(|parent| {
+                    //IMG 1
+                    parent.spawn(ImageBundle {
+                        style: IMAGE_STYLE,
+                        image: asset_server.load("sprites/ball_blue_large.png").into(),
+                        ..default()
+                    });
+
+                    //TEXT
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "Bevy Ball Game",
+                                get_button_text_style(asset_server, 64.0),
+                            )],
+                            alignment: TextAlignment::Center,
+                            ..default()
+                        },
+                        ..default()
+                    });
+
+                    //IMG 2
+                    parent.spawn(ImageBundle {
+                        style: IMAGE_STYLE,
+                        image: asset_server.load("sprites/ball_red_large.png").into(),
+                        ..default()
+                    });
+                });
+
+            //PLAY
+            parent
+                .spawn((
+                    ButtonBundle {
+                        style: BUTTON_STYLE,
+                        background_color: NORMAL_BUTTON_COLOR.into(),
+                        ..default()
+                    },
+                    PlayButton {},
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "Play",
+                                get_button_text_style(asset_server, 32.0),
+                            )],
+                            alignment: TextAlignment::Center,
+                            ..default()
+                        },
+                        ..default()
+                    });
+                });
+
+            //QUIT
+            parent
+                .spawn((
+                    ButtonBundle {
+                        style: BUTTON_STYLE,
+                        background_color: NORMAL_BUTTON_COLOR.into(),
+                        ..default()
+                    },
+                    QuitButton {},
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "Quit",
+                                get_button_text_style(asset_server, 32.0),
+                            )],
+                            alignment: TextAlignment::Center,
+                            ..default()
+                        },
+                        ..default()
+                    });
+                });
+        })
+        .id();
+
+    main_menu_entity
+}
